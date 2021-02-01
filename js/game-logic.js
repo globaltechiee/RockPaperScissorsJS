d5a5e6e920034413df4cf245d31ee3fa43f30135
cutty12 <cuttyswiss@protonmail.com>
2021-02-01 16:30:13 +0100

    first commit

---
+++ js/game-logic.js
@@ -0,0 +1,21 @@
+// All code should be written in this file.
+
+let playerOneMoveOneType,
+    playerOneMoveTwoType,
+    playerOneMoveThreeType,
+    playerTwoMoveOneType,
+    playerTwoMoveTwoType,
+    playerTwoMoveThreeType,
+    playerOneMoveOneValue,
+    playerOneMoveTwoValue,
+    playerOneMoveThreeValue,
+    playerTwoMoveOneValue,
+    playerTwoMoveTwoValue,
+    playerTwoMoveThreeValue
+// create 'setPlayerMoves()' and pass in para,
+// player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue
+
+const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue) => {
+
+    // Creat a failfast
+}
